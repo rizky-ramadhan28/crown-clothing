cf7b8ca62d08da4db61fa770a8bffa7068eef2f4
Rizky Ramadhan <rizkydorable48@gmail.com>
2020-05-09 00:23:13 +0700

    Add sign in and sign up.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,26 @@ const config = {
   measurementId: "G-X9NGXQ806L",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`/users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log(`Error creating user ${error.message}`);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 const provider = new firebase.auth.GoogleAuthProvider();
